18e8cef4bddb2431a30fd98e46d78b906d552f07
Pedro Ferragut <75221107+peferragut@users.noreply.github.com>
2023-05-23 12:23:20 -0300

    Create files and page structure

---
+++ to-do-list/src/App.tsx
@@ -0,0 +1,14 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <h1>To-do List</h1>
+      <div className={styles.body_page}>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
